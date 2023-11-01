f3e819e0a5e7587c27b73724e7d5e3825ba4617b
xilefenko <xilefenko@hotmail.com>
2023-11-01 17:01:38 +0100

    Climbing

---
+++ Source/PORL/CustomCode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "CustomCode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PORL_API UCustomCode : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
